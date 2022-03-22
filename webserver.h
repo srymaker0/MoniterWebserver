ee7f73490e7979e3a58da815bb0bbb733b2db1e2
srymaker0 <zyjay0@gmail.com>
2022-03-22 18:05:29 +0800

    feat: webserver head file

---
+++ webserver.h
@@ -0,0 +1,88 @@
+/*************************************************************************
+	> File Name: webserver.h
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 22 Mar 2022 03:01:05 PM CST
+ ************************************************************************/
+
+#ifndef _WEBSERVER_H
+#define _WEBSERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+#include <cassert>
+
+#include "./threadpool/threadpool.h"
+#include "./http/http_conn.h"
+
+const int MAX_FD = 65535;
+const int MAX_EVENT_NUMBER = 10000;
+const int TIMESLOT = 5;
+
+class Webserver {
+public:
+    WebServer();
+    ~WebServer();
+    
+    void init(int port, string user, string passWord, string databaseName,
+              int log_write, int opt_linger, int trigmode, int sql_num,
+              int thread_num, int close_log, int actor_model);
+
+    void thread_pool();
+    void sql_pool();
+    void log_write();
+    void trig_mode();
+    void eventListen();
+    void eventLoop();
+    void timer(int connfd, struct sockaddr_in client_address);
+    void adjust_timer(util_timer *timer);
+    void deal_timer(util_timer *timer, int sockfd);
+    bool dealclinetdata();
+    bool dealwithsignal(bool &timeout, bool &stop_server);
+    void dealwithread(int sockfd);
+    void dealwithwrite(int sockfd);
+
+public:
+    int m_port;
+    char *m_root;
+    int m_log_write;
+    int m_close_log;
+    int m_actormodel;
+
+    int m_pipefd[2];
+    int m_epollfd;
+    http_conn *user;
+
+    // mysql
+    connection_poll *m_connPool;
+    string m_user;
+    string m_passWord;
+    string m_databaseName;
+    int m_sql_num;
+
+    // thread_pool
+    threadpool<http_conn> *m_pool;
+    int m_thread_num;
+
+    epoll_event events[MAX_EVENT_NUMBER];
+
+    int m_listenfd;
+    int m_OPT_LINGER;
+    int m_TRIGMode;
+    int m_LISTENTrigmode;
+    int m_CONNTrigmode;
+
+    client_data *users_timer;
+    Utils utils;
+
+};
+
+
+
+#endif
