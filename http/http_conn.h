57857be26377775471bdfffbdaf412ef6c63a792
srymaker0 <zyjay0@gmail.com>
2022-03-25 17:29:03 +0800

    feat: http connection

---
+++ http/http_conn.h
@@ -0,0 +1,35 @@
+/*************************************************************************
+	> File Name: http_conn.h
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 25 Mar 2022 03:13:15 PM CST
+ ************************************************************************/
+
+#ifndef _HTTP_CONN_H
+#define _HTTP_CONN_H
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/uio.h>
+#include <map>
+
+#include "../lock/locker.h"
+#include "../CGImysql/sql_connection_pool.h"
+#include "../log/log.h"
+
+#endif
