eaf207c407ac1de5959b37090f7938042806620c
srymaker0 <zyjay0@gmail.com>
2022-03-28 22:26:24 +0800

    feat: add do_request function

--- http/http_conn.cpp
+++ http/http_conn.cpp
@@ -138,7 +138,7 @@ http_conn::LINE_STATUS http_conn::parse_line() {
     for (; m_checked_idx < m_read_idx; ++m_checked_idx) {
         temp = m_read_buf[m_checked_idx];
         if (temp == '\r') {
-            if ((m_check_idx + 1) == m_read_idx) {
+            if ((m_checked_idx + 1) == m_read_idx) {
                 return LINE_OPEN;
             } else if (m_read_buf[m_checked_idx + 1] == '\n') {
                 m_read_buf[m_checked_idx++] = '\0';
@@ -147,7 +147,7 @@ http_conn::LINE_STATUS http_conn::parse_line() {
             }
             return LINE_BAD;
         } else if (temp == '\n') {
-            if (m_check_idx > 1 && m_read_buf[m_checked_idx - 1] == '\r') {
+            if (m_checked_idx > 1 && m_read_buf[m_checked_idx - 1] == '\r') {
                 m_read_buf[m_checked_idx - 1] = '\0';
                 m_read_buf[m_checked_idx++] = '\0';
                 return LINE_OK;
@@ -299,7 +299,7 @@ http_conn::HTTP_CODE http_conn::process_read() {
 http_conn::HTTP_CODE http_conn::do_request() {
     strcpy(m_real_file, doc_root);
     int len = strlen(doc_root);
-    const char *p = sttrrchr(m_url, '/');
+    const char *p = strrchr(m_url, '/');
     if (cgi == 1 && (*(p + 1) == '2' || *(p + 1) == '3')) {
         char flag = m_url[1];
         char *m_url_real = (char *)malloc(sizeof(char) * 200);
@@ -321,7 +321,7 @@ http_conn::HTTP_CODE http_conn::do_request() {
         password[j] = '\0';
 
         if (*(p + 1) == '3') {
-            char *sql_insertt = (char *)malloc(sizeof(char) * 200);
+            char *sql_insert = (char *)malloc(sizeof(char) * 200);
             strcpy(sql_insert, "INSERT INTO user(username, passwd) VALUES(");
             strcat(sql_insert, "'");
             strcat(sql_insert, name);
@@ -350,9 +350,37 @@ http_conn::HTTP_CODE http_conn::do_request() {
                 strcpy(m_url, "/logError.html");
             }
         }
-
-
     }
+        
+    if (*(p + 1) == '0') {
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/register.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+        free(m_url_real);
+    } else if (*(p + 1) == '1') {
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/log.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+        free(m_url_real);
+    } else if (*(p + 1) == '.') {
+        // ..........
+        //
+        //
+        // ..........
+    } else {
+        strncpy(m_real_file + len, m_url, FILENAME_LEN - len - 1);
+    }
+
+    if (stat(m_real_file, &m_file_stat) < 0) return NO_REQUEST;
+
+    if (!(m_file_stat.st_mode & S_IROTH)) return FORBIDDEN_REQUEST;
+
+    if (S_ISDIR(m_file_stat.st_mode)) return BAD_REQUEST;
+
+    int fd = open(m_real_file, O_RDONLY);
+    m_file_address = (char *)mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+    return FILE_REQUEST;
 }
 
 
