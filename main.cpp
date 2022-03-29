e9b80ba17aed64a67f4fe630ffe47c85407a7d81
srymaker0 <zyjay0@gmail.com>
2022-03-29 22:22:08 +0800

    feat: just main

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 29 Mar 2022 10:18:18 PM CST
+ ************************************************************************/
+
+#include "config.h"
+
+int main(int argc, char *argv[]) {
+    string user = "root";
+    string passwd = "ZYJadmin@123";
+    string databasename = "srydb";
+
+    Config config;
+    config.parse_arg(argc, argv);
+    WebServer server;
+    server.init(config.PORT, user, passwd, databasename, config.LOGWrite, config.OPT_LINGER, config.TRIGMode, config.sql_num, config.thread_num, config.close_log, config.actor_model);
+    
+    server.log_write();
+    server.sql_pool();
+    server.thread_pool();
+    server.trig_mode();
+    server.eventListen();
+    server.eventLoop();
+    return 0;
+}
