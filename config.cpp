fdfc170361107bee6a67007af74f658372a6ae38
srymaker0 <zyjay0@gmail.com>
2022-03-31 22:55:16 +0800

    fix: port

--- config.cpp
+++ config.cpp
@@ -7,8 +7,37 @@
 
 #include "config.h"
 
-Config::Config() : PORT(7777), LOGWrite(0), TRIGMode(0), LISTENTrigmode(0),
-CONNTrigmode(0), OPT_LINGER(0), sql_num(8), thread_nums(8), close_log(0), actor_model(0) {}
+Config::Config() {
+    //端口号,默认7777
+    PORT = 7777;
+
+    //日志写入方式，默认同步
+    LOGWrite = 0;
+
+    //触发组合模式,默认listenfd LT + connfd LT
+    TRIGMode = 0;
+
+    //listenfd触发模式，默认LT
+    LISTENTrigmode = 0;
+
+    //connfd触发模式，默认LT
+    CONNTrigmode = 0;
+
+    //优雅关闭链接，默认不使用
+    OPT_LINGER = 0;
+
+    //数据库连接池数量,默认8
+    sql_num = 8;
+
+    //线程池内的线程数量,默认8
+    thread_nums = 8;
+
+    //关闭日志,默认不关闭
+    close_log = 0;
+
+    //并发模型,默认是proactor
+    actor_model = 0;
+}
 
 void Config::parse_arg(int argc, char *argv[]) {
     int opt;
