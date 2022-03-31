99e9c15fec73aadaf65a8582318445da36af4a7a
srymaker0 <zyjay0@gmail.com>
2022-03-31 22:54:42 +0800

    feat: deebug

--- main.cpp
+++ main.cpp
@@ -15,13 +15,27 @@ int main(int argc, char *argv[]) {
     Config config;
     config.parse_arg(argc, argv);
     WebServer server;
+    printf("server init...\n");
+    
     server.init(config.PORT, user, passwd, databasename, config.LOGWrite, config.OPT_LINGER, config.TRIGMode, config.sql_num, config.thread_nums, config.close_log, config.actor_model);
     
+    //printf("log write...\n");
     server.log_write();
+
+    //printf("sql pool...\n");
     server.sql_pool();
+
+    //printf("thread pool...\n");
     server.thread_pool();
+
+    //printf("trig mode...\n");
     server.trig_mode();
+
+    //printf("eventListen...\n");
     server.eventListen();
+    
+    printf("eventLoop...\n");
     server.eventLoop();
+    //printf("eventLoop after........\n");
     return 0;
 }
